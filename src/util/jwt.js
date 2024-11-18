d4a1d57b4c412a22f6930739afff3e083b0686ff
Ayrton Rodrigues <161531292+ayrtonbq@users.noreply.github.com>
2024-11-18 15:06:26 -0300

    Update jwt.js

--- src/util/jwt.js
+++ src/util/jwt.js
@@ -1,14 +1,18 @@
 import 'dotenv/config';
-import jwt from "jsonwebtoken";
+import jwt from 'jsonwebtoken';
+
+// Utilize a variável de ambiente AUTH_SECRET
+const authSecret = process.env.AUTH_SECRET;
 
 function sign(data) {
-    const token = jwt.sign(data, '~e.3x2<o2i#y_e~08z?d*!w~(g:qrl1e,w;{,e<hphhl,1e!@v(1].*d>l`kfpj', { expiresIn: '10d' })
+    // Use a chave secreta do .env para criar o token
+    const token = jwt.sign(data, authSecret, { expiresIn: '10d' });
     return token;
 }
 
 function verify(token) {
-
-    return jwt.verify(token, '~e.3x2<o2i#y_e~08z?d*!w~(g:qrl1e,w;{,e<hphhl,1e!@v(1].*d>l`kfpj', function (err, decoded) {
+    // Use a chave secreta do .env para verificar o token
+    return jwt.verify(token, authSecret, function (err, decoded) {
         if (err) return null;
 
         return decoded;
