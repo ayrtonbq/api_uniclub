993b2814bc984563b2dd2e820c6424268c81e2d3
Ayrton Rodrigues <161531292+ayrtonbq@users.noreply.github.com>
2024-11-22 09:07:28 -0300

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -4,12 +4,13 @@ import pg from 'pg';
 const { Pool } = pg;
 
 // Configurações de conexão para PostgreSQL usando variáveis de ambiente
+
 const pool = new Pool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_PATH,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
+    host: node206951-uniclub.sp1.br.saveincloud.net.br,
+    port: 11088,
+    database: uniclub,
+    user: uniclub,
+    password: "#abc123",
     max: 10,
     idleTimeoutMillis: 30000,
 });
@@ -26,19 +27,23 @@ pool.connect((err, client, release) => {
 
 // Função para ler dados do banco de dados PostgreSQL
 async function read(ssql, params) {
+    console.log("Executando consulta SQL:", ssql);
+    console.log("Parâmetros da consulta:", params);
+    
     const client = await pool.connect();
     try {
         const result = await client.query(ssql, params);
         
         if (result.rows.length === 0) {
-            console.warn("Query returned no results."); 
+            console.warn("Consulta não retornou resultados."); 
         } else {
+            console.log("Consulta executada com sucesso. Resultados:", result.rows);
         }
 
         return result.rows; 
     } catch (err) {
         console.error('Erro na consulta:', err.message); 
-        throw new Error('Database query failed: ' + err.message); 
+        throw new Error('Erro na consulta ao banco de dados: ' + err.message); 
     } finally {
         client.release(); 
     }
@@ -46,15 +51,25 @@ async function read(ssql, params) {
 
 // Função para escrever dados no banco de dados PostgreSQL
 async function write(ssql, params) {
+    console.log("Executando SQL de escrita:", ssql);
+    console.log("Parâmetros da escrita:", params);
+    
     const client = await pool.connect();
     try {
         const result = await client.query(ssql, params);
+        console.log("Escrita executada com sucesso:", result);
         return result;
     } catch (err) {
-        throw err; // Lançar o erro para tratamento posterior
+        console.error('Erro na escrita:', err.message);
+        throw new Error('Erro na escrita ao banco de dados: ' + err.message); 
     } finally {
         client.release();
     }
 }
+
+// Eventos adicionais de conexão do pool
+pool.on('error', (err) => {
+    console.error('Erro inesperado no pool:', err);
+});
 
 export { read, write };
